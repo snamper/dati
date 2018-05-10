70b7f6b8e466ada2ba735c2fb880ee06c3820044
shingli <745573545@qq.com>
2018-05-10 22:05:00 +0800

    classMap

--- src/page/ret/ret.vue
+++ src/page/ret/ret.vue
@@ -4,7 +4,7 @@
         <!-- 结果类型 -->
         <div class="dialog_wrap">
             <div class="dialog">
-                <p class="text">快乐型</p>
+                <p class="text">{{type[currenType.id-1]}}</p>
             </div>
         </div>
         <!-- 结果的统计 -->
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="right_wrap">
-
+                <i class="icon" :class='classMap[currenType.id-1]'></i>
             </div>
         </div>
     </div>
@@ -36,18 +36,26 @@
         name:'result' ,
         data(){
             return {
-
+                currenType:{
+                    id:1
+                },
+                type:['完美型','爱心型','成就型','自我型','理智型','疑惑型','快乐型','领袖型','和平型']
             }
         },
         created(){
             
             // 进入页面的时候请求数据
+            // 这个是根据后台传过来的数据动态设置类型的图片
+            this.classMap =['perfect','love','achievement','self','rational','confusion','happy','leader','peace'];
 
         }
     }
 </script>
 
 <style lang="less" scoped>
+    .bg(@url){
+        background-image:url('../../assets/image/@{url}.png')
+    }
     .result_wrap{
         position: fixed;
         top:0;
@@ -104,6 +112,50 @@
             .right_wrap{
                 width: 15rem;
                 height: 15rem;
+                .icon{
+                    display:inline-block;
+                    width:15rem;
+                    height:15rem;
+                    background-size:15rem 15rem;
+                    background-repeat:no-repeat;
+                    &.perfect{
+                        // 完美
+                        .bg(1)
+                    }
+                    &.love{
+                        // 爱心
+                        .bg(2)
+                    }
+                    &.achievement{
+                        // 成就
+                        .bg(3)
+                    }
+                    &.self{
+                        // 自我
+                        .bg(4)
+                    }
+                    &.rational{
+                        // 理智
+                        .bg(5)
+                    }
+                    &.confusion{
+                        // 疑惑
+                        .bg(6)
+                    }
+                    &.happy{
+                        // 开心
+                        .bg(7)
+                    }
+                    &.leader{
+                        // 领袖
+                        .bg(8)
+                    }
+                    &.peace{
+                        // 和平
+                        .bg(9)
+                    }
+
+                }
             }
         }
     }
